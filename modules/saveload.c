3a617d943e571af3c37c16395b6ee26d8cec3463
ADDRMeridan <ADDRMeridan@users.noreply.github.com>
2017-05-08 17:56:02 +0200

    Add files via upload

--- modules/saveload.c
+++ modules/saveload.c
@@ -1,5 +1,5 @@
 /**************************************************************
-* saveload.c - Last modified: 06/05/2017                      * *                                                             *
+* saveload.c - Last modified: 08/05/2017                      * *                                                             *
 * Allows saving a Game in a file and loading a Game from file *
 *                                                             *
 * Mael "ADDRMeridan" MOULIN				      *
@@ -12,14 +12,20 @@
 #include <sys/stat.h>
 
 #include "game.h"
+#include "saveload.h"
 
 void saveGame(char *filename, Game g) {
 
 	mkdir("../savegame", S_IRWXU);
+	//making path name
+	char path[97];	//filename <= 80 char
+	strcpy(path, "../savegame/");
+	strcat(path, filename);
+	strcat(path, ".txt");
 	//Open file
-	FILE *f = fopen(strcat("../savegame/",filename), "wt"); //SEGMENTATION FAULT HERE
+	FILE *f = fopen(path, "wt");
 	if(f == NULL) {
-		perror(filename);
+		perror(path);
 		exit(EXIT_FAILURE);
 	}
 	//File header
@@ -52,6 +58,7 @@ void saveGame(char *filename, Game g) {
 		}
 	}
 	fprintf(f, "\\endboard\n");
+	//Saving History
 	fprintf(f, "\\game\n");
 	for(int i = 0; i < getNbCoup(g); ++i) {
 		Coup cp = getCoupFromHist(i, g);
@@ -78,16 +85,129 @@ void saveGame(char *filename, Game g) {
 	fclose(f);
 }
 
-//TO REMOVE BEFORE PULLING
-int main(int argc, char *argv[]) {
+//Returns NULL if something's wrong
+static Coup loadCoup(char *line) {
 
-	Game g = createGame(RED, 9);
-	Coup cp = createCoup(RED, 5, 5);
-	g = addCoupToBoard(cp, g);
-	cp = createCoup(BLUE, 0, 0);
-	g = addCoupToBoard(cp, g);
-	char *filename = "game1.txt";
-	saveGame(filename, g);
-	deleteGame(g);
-	return 0;
+	Coup cp;
+	char *wrd = NULL;	
+	wrd = strtok(line, " ");
+	if(strcmp(wrd, "\\play")) {
+		return NULL;
+	} else {
+		//Get color
+		wrd = strtok(NULL, " ");
+		Color tmpC;
+		switch(wrd[0]) {
+			case 'R':
+				tmpC = RED;
+				break;
+			case 'B':
+				tmpC = BLUE;
+				break;
+			default:
+				return NULL;
+		}
+		//Get X Axis
+		wrd = strtok(NULL, " ");
+		if(wrd == NULL) {
+			return NULL;
+		}
+		int tmpX = strtol(wrd, NULL, 10);
+		//Get Y Axis
+		wrd = strtok(NULL, " ");
+		if(wrd == NULL) {
+			return NULL;
+		}
+		int tmpY = strtol(wrd, NULL, 10);
+		//Create Coup
+		cp = createCoup(tmpC, tmpX, tmpY);	
+	}
+	return cp;
+}
+
+Game loadGame(char *filename) {
+
+	//making path name
+	char path[97];	//filename <= 80 char
+	strcpy(path, "../savegame/");
+	strcat(path, filename);
+	strcat(path, ".txt");
+	//Open File
+	FILE *f = fopen(path, "rt");
+	if(f == NULL) {
+		perror(path);
+		return NULL;
+	}
+	//Init readline
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nbChar;
+	//Check if hex
+	nbChar = getline(&line, &len, f);
+	if(nbChar == -1 || strcmp(line, "\\hex\n")) {
+		fprintf(stderr, "loadGame: Wrong file type.\n");
+		return NULL;
+	}
+	//Get dimension
+	nbChar = getdelim(&line, &len, ' ', f);
+	if(nbChar == -1 || strcmp(line, "\\dim ")) {
+		fprintf(stderr, "loadGame: corrupted save.\n");
+		return NULL;
+	}
+	int tmp;
+	fscanf(f, "%d", &tmp);
+	if(tmp < 2) {
+		fprintf(stderr, "loadGame: board size too small.\n");
+		return NULL;
+	}
+	nbChar = getline(&line, &len, f); //Flush '\n'
+	//Check board
+	nbChar = getline(&line, &len, f);
+	if(nbChar == -1 || strcmp(line, "\\board\n")) {
+		printf("Here\n");
+		fprintf(stderr, "loadGame: corrupted save.\n");
+		return NULL;
+	}
+	//Skipping board representation
+	for(int i = 0; i < tmp; ++i) {
+		nbChar = getline(&line, &len, f);
+	}
+	//Checking end board
+	nbChar = getline(&line, &len, f);
+	if(nbChar == -1 || strcmp(line, "\\endboard\n")) {
+		fprintf(stderr, "loadGame: corrupted save.\n");
+		return NULL;
+	}
+	//Check history
+	nbChar = getline(&line, &len, f);
+	if(nbChar == -1 || strcmp(line, "\\game\n")) {
+		fprintf(stderr, "loadGame: corrupted save.\n");
+		return NULL;
+	}
+	Game g = createGame(EMPTY, tmp);
+	//EMPTY is changed when the first Coup is added
+	nbChar = getline(&line, &len, f);
+	while(nbChar != -1 && strcmp(line, "\\endgame\n")) {
+		Coup cp = loadCoup(line);
+		if(cp == NULL) {
+			nbChar = -1;
+		} else {
+			g = addCoupToBoard(cp, g);
+			nbChar = getline(&line, &len, f);
+		}
+	}
+	if(nbChar == -1 || strcmp(line, "\\endgame\n")) {
+		fprintf(stderr, "loadGame: corrupted save.\n");
+		deleteGame(g);
+		return NULL;
+	}
+	//Check end of hex
+	nbChar = getline(&line, &len, f);
+	if(nbChar == -1 || strcmp(line, "\\endhex\n")) {
+		fprintf(stderr, "loadGame: corrupted save.\n");
+		deleteGame(g);
+		return NULL;
+	}
+	fclose(f);
+	return g;
 }
--- modules/saveload.h
+++ modules/saveload.h
@@ -2,12 +2,19 @@
 #define SAVELOAD_H
 
 /**************************************************************
-* saveload.h - Last modified: 17/04/2017                      * *                                                             *
+* saveload.h - Last modified: 08/05/2017                      * *                                                             *
 * Allows saving a Game in a file and loading a Game from file *
 *                                                             *
 * Mael "ADDRMeridan" MOULIN				      *
 **************************************************************/
 
-void saveGame(Game g);
+//Saves a currently existing game
+//filename must be <= than 80 characters
+void saveGame(char *filename, Game g);
+
+//Loads a saved Game
+//filename must be <= than 80 characters
+//Returns NULL if something went wrong;
+Game loadGame(char *filename);
 
 #endif

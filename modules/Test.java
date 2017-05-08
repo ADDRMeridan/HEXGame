908b71f1bdec36a2bb75d7b51f215733c5d95385
ADDRMeridan <ADDRMeridan@users.noreply.github.com>
2017-05-08 17:48:56 +0200

    Delete Test.java

--- modules/Test.java
+++
@@ -1,9 +0,0 @@
-
-public class Test {
-	public static void main(String[] args) {
-		Interface i=new Interface();
-		i.SaisieTour('x',1);
-		i.SaisieTour('o',2);
-		i.getPlateau().getPlateau();
-	}
-}

ce615cc29f2772f2c75773e9effc7afba34050c4
M-D75 <mourdasm13@gmail.com>
2017-05-12 09:35:41 +0200

    Update graphe.h

--- modules/graphe.h
+++ modules/graphe.h
@@ -34,6 +34,19 @@ typedef struct s_hexa * Hexa;
 Graphe inite_graphe(int n);
 
 /**
+ *	@brief colorie un Hexagone (sommet).
+ *
+ *	@param sommet : le sommet à colorier
+ *	@param couleur : la couleur de l'hexagone, 0 = NOIR; 1 = BLANC. 
+ *
+ *	@pre-cond : couleur = 0 ou couleur = 1 (BLANC ou NOIR ou TRANSPARANT)
+ *
+ *  @return un Hexa (sommet) modifier.
+ *
+ */
+Hexa colorie_sommet(Hexa sommet, int couleur);
+
+/**
  *	@brief ajout un hexagone de la couleur indiquer.
  *
  *	@param graphe : le Graphe au quelle on shouaite ajouter un hexagone
@@ -50,6 +63,7 @@ Graphe inite_graphe(int n);
  *
  */
 Graphe ajout_hexagone(Graphe graphe, int row, int colonne, int couleur);
+
 
 Graphe graphe_reduit( Graphe graphe );
 
@@ -68,19 +82,40 @@ Graphe graphe_reduit( Graphe graphe );
 int size_graphe(Graphe graphe);
 
 /**
- *	@brief indique la couleur d'un hexagone se trouvant dans un 
- 	Graphe à un certain emplacement.
+ *	@brief renvoie un hexagone (sommet), d'un graphe 
+ 	selon la ligne et la colonne donnée.
  *
  *	@param graphe : le Graphe en question
  *	@param row : la ligne
  *	@param colonne : la colonne
  *
- *	@pre-cond : 0 <= row < size_graphe( graphe ); && 0 <= col < size_graphe( graphe );
+ *	@pre-cond : 0 <= row < size_graphe( graphe ); && 0 <= colonne < size_graphe( graphe );
+ *
+ *  @return Hexa : un sommet a la position [row, colonne] dans le graphe.
+ *
+ */
+Hexa hexagone(Graphe graphe, int row, int colonne);
+
+/**
+ *	@brief indique la couleur d'un hexagone se trouvant dans un 
+ 	Graphe à un certain emplacement.
+ *
+ *	@param hex : l'hexagone (sommet) en question
  *
  *  @return un entier ( 0, 1 ou 2 ) indiquant la couleur, : 2 = TRANSPARANT; 1 = BLANC; 0 = NOIR.
  *
  */
-int couleur_hexagone(Graphe graphe, int row, int colonne);
+int couleur_sommet(Hexa hex);
+
+/**
+ *	@brief fourni le groupe d'un sommet.
+ *
+ *	@param sommet : le sommet en question
+ *
+ *  @return Hexa : un groupe (sommet) si il en a un, sinon retourne NULL.
+ *
+ */
+Hexa groupe(Hexa sommet);
 
 /**
  *	@brief indique la couleur d'un hexagone se trouvant dans un 
@@ -96,8 +131,43 @@ int couleur_hexagone(Graphe graphe, int row, int colonne);
  */
 bool chaine_gagnante(Graphe graphe, int couleur);
 
+/**
+ *	@brief : indique la distance entre deux hexagones (sommet)
+ 		correspond au nombre d’hexagones qui les séparent par le chemin le plus court.
+ *
+ *	@param p : premier hexagone
+ *	@param q : second hexagone
+ *
+ *  @return int : un entier indiquant la distance entre les deux hexagones (p,q),
+ 		notez d(p,q)
+ 		d(p,q) = au nombre d’hexagones qui les séparent par le chemin le plus court.
+ *
+ */
+int distance_hexagones(Hexa p, Hexa q);
 
-int distance_hexa(Graphe graphe, int row_1, int col_1, int row_2, int col_2);
+/**
+ *	@brief cherche la distance minimal entre un sommet et un groupe.
+ *
+ *	@param p : le sommet concernet
+ *	@param groupe : le groupe concernet
+ *
+ *	@pre-condition : groupe != NULL
+ *
+ *  @return int : un entier corespondant a la distance minimale.
+ *
+ */
+int distance_hexagone_groupe( Hexa p, Hexa groupe);
+
+/**
+ *	@brief indique si deux hexagone sont adjacent (se touche).
+ *
+ *	@param hex_1 : le premier hexagone
+ *	@param hex_2 : le second hexagone 
+ *
+ *  @return boolean : true si il sont adjacent sinon false.
+ *
+ */
+bool hex_adjacent(Hexa hex_1, Hexa hex_2);
 
 /* */
 void affiche_sommet_hexa(Graphe graphe);

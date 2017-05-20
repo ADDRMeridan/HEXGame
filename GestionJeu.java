0dec3be8dbe3f590896e2226476c9d02c7922d28
M-D75 <mourdasm13@gmail.com>
2017-05-20 22:08:49 +0200

    Add files via upload

---
+++ GestionJeu.java
@@ -0,0 +1,16 @@
+package gestionJeu;
+
+public class GestionJeu{
+	static{
+		System.loadLibrary("JavaGestionJeu");
+	}
+	public static native void nouvellePartie(char couleur, int taille); 
+	public static native char couleurCase(int x, int y);
+	public static native boolean coupValide(int x, int y, char couleur);
+	public static native boolean gagnant(char couleur);
+	public static native void supprPartie();
+	public static native boolean annuleCoup();
+	public static native int nbTourPartie();
+	public static native boolean ContreOrdinateur();
+	public static native char ProchainJoueur();
+}
